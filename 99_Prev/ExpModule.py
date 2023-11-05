4d2d2318f6b0cb5191203f9a9699786dfc3e7fab
nuyhc <spec327@naver.com>
2023-11-05 21:38:01 +0900

    add

--- 99_Prev/ExpModule.py
+++ 99_Prev/ExpModule.py
@@ -27,7 +27,7 @@ x_train, x_val, y_train, y_val = train_test_split(data[ftrs], data["Y"], stratif
 train = pd.concat([x_train, y_train], axis=1).reset_index(drop=True)
 valid = pd.concat([x_val, y_val], axis=1).reset_index(drop=True)
 #
-data_n = 3
+data_n = 5
 #
 # RFE 이용해, n개의 피처 선택
 def select_n_ftrs(model, n, data=train, ftrs=ftrs, target="Y"):
@@ -41,11 +41,11 @@ def select_n_ftrs(model, n, data=train, ftrs=ftrs, target="Y"):
 # 모델 훈련
 class ModelTrain:
     @staticmethod
-    def statics(model, sample_data, selected_ftrs, target="Y"):
+    def statics(model, sample_data, selected_ftrs=ftrs, target="Y"):
         model.fit(sample_data[selected_ftrs], sample_data[target])
         return model.predict(x_val[selected_ftrs])
     @staticmethod
-    def kmeans(model, sample_data, selected_ftrs):
+    def kmeans(model, sample_data, selected_ftrs=ftrs):
         model.fit(sample_data[selected_ftrs])
         train_result = pd.concat([sample_data[selected_ftrs].reset_index(drop=True), sample_data["Y"].reset_index(drop=True), pd.DataFrame({"Train_Cluster":model.labels_})], axis=1)
         pred_result = pd.concat([x_val[selected_ftrs].reset_index(drop=True), y_val.reset_index(drop=True), pd.DataFrame({"Pred_Cluster":model.predict(x_val[selected_ftrs].values)})], axis=1)
@@ -84,28 +84,32 @@ def train_n(n, sn, data=train, target="Y"):
         print(sample.shape[0], end=" ", sep=" ")
         #
         lr = LinearRegression()
-        dt = DecisionTreeRegressor(random_state=42)
-        rf = RandomForestRegressor(random_state=42)
+        dt = DecisionTreeRegressor(random_state=42, max_depth=4)
+        rf = RandomForestRegressor(random_state=42, max_depth=4)
         km = KMeans(n_clusters=n, init="k-means++")
         #
-        sftr_lr = select_n_ftrs(lr, sn)
-        sftr_dt = select_n_ftrs(dt, sn)
-        sftr_rf = select_n_ftrs(rf, sn)
+        # sftr_lr = select_n_ftrs(lr, sn)
+        # sftr_dt = select_n_ftrs(dt, sn)
+        # sftr_rf = select_n_ftrs(rf, sn)
         #
-        statics_selected = pd.DataFrame({"Selected":sftr_lr+sftr_dt+sftr_rf})
-        statics_selected = pd.DataFrame(statics_selected.groupby("Selected")["Selected"].count())
-        statics_selected = statics_selected.rename_axis("Ftrs").reset_index(drop=False).sort_values("Selected", ascending=False)
-        sftr_k = statics_selected["Ftrs"].tolist()[:sn]
+        # statics_selected = pd.DataFrame({"Selected":sftr_lr+sftr_dt+sftr_rf})
+        # statics_selected = pd.DataFrame(statics_selected.groupby("Selected")["Selected"].count())
+        # statics_selected = statics_selected.rename_axis("Ftrs").reset_index(drop=False).sort_values("Selected", ascending=False)
+        # sftr_k = statics_selected["Ftrs"].tolist()[:sn]
         #
-        pred_lrs.append(ModelTrain.statics(lr, sample, sftr_lr))
-        pred_dts.append(ModelTrain.statics(dt, sample, sftr_dt))
-        pred_rfs.append(ModelTrain.statics(rf, sample, sftr_rf))
-        pred_kmeans.append(ModelTrain.kmeans(km, sample, sftr_k))
+        pred_lrs.append(ModelTrain.statics(lr, sample))
+        pred_dts.append(ModelTrain.statics(dt, sample))
+        pred_rfs.append(ModelTrain.statics(rf, sample))
+        pred_kmeans.append(ModelTrain.kmeans(km, sample))
+        # pred_lrs.append(ModelTrain.statics(lr, sample, sftr_lr))
+        # pred_dts.append(ModelTrain.statics(dt, sample, sftr_dt))
+        # pred_rfs.append(ModelTrain.statics(rf, sample, sftr_rf))
+        # pred_kmeans.append(ModelTrain.kmeans(km, sample, sftr_k))
         #
-        sftrs_lrs.append(sftr_lr)
-        sftrs_dts.append(sftr_dt)
-        sftrs_rfs.append(sftr_rf)
-        sftrs_kmeans.append(sftr_k)
+        # sftrs_lrs.append(sftr_lr)
+        # sftrs_dts.append(sftr_dt)
+        # sftrs_rfs.append(sftr_rf)
+        # sftrs_kmeans.append(sftr_k)
     # 샘플링 데이터 시각화
     for _ in range(len(sample_data)): sns.kdeplot(sample_data[_]["Y"], fill=True, color=color_map[f"Trial {_}"])
     plt.legend(color_map.keys())
@@ -126,19 +130,22 @@ def plot_static(result, n, sn):
     ax[0].plot(y_val.values, color="black", linestyle=":") # 원본
     ax[0].plot(pd.DataFrame(result["LR"]["P"]).mean(axis=0).values, color="r")
     r2 = r2_score(y_val.values, pd.DataFrame(result["LR"]["P"]).mean(axis=0).values)
-    mape = mean_absolute_percentage_error(y_val.values, pd.DataFrame(result["LR"]["P"]).mean(axis=0).values)
+    mape = (abs(y_val.values - pd.DataFrame(result["LR"]["P"]).mean(axis=0).values) / y_val.values).mean()
+    # mape = mean_absolute_percentage_error(y_val.values, pd.DataFrame(result["LR"]["P"]).mean(axis=0).values)
     ax[0].set_title(f"LR\nM.R2: {r2:.4f}\nM.MAPE: {mape:.4f}")
     #
     ax[1].plot(y_val.values, color="black", linestyle=":") # 원본
     ax[1].plot(pd.DataFrame(result["DT"]["P"]).mean(axis=0).values, color="r")
     r2 = r2_score(y_val.values, pd.DataFrame(result["DT"]["P"]).mean(axis=0).values)
-    mape = mean_absolute_percentage_error(y_val.values, pd.DataFrame(result["DT"]["P"]).mean(axis=0).values)
+    # mape = mean_absolute_percentage_error(y_val.values, pd.DataFrame(result["DT"]["P"]).mean(axis=0).values)
+    mape = (abs(y_val.values - pd.DataFrame(result["DT"]["P"]).mean(axis=0).values) / y_val.values).mean()
     ax[1].set_title(f"DT\nM.R2: {r2:.4f}\nM.MAPE: {mape:.4f}")
     #
     ax[2].plot(y_val.values, color="black", linestyle=":") # 원본
     ax[2].plot(pd.DataFrame(result["RF"]["P"]).mean(axis=0).values, color="r")
     r2 = r2_score(y_val.values, pd.DataFrame(result["RF"]["P"]).mean(axis=0).values)
-    mape = mean_absolute_percentage_error(y_val.values, pd.DataFrame(result["RF"]["P"]).mean(axis=0).values)
+    # mape = mean_absolute_percentage_error(y_val.values, pd.DataFrame(result["RF"]["P"]).mean(axis=0).values)
+    mape = (abs(y_val.values - pd.DataFrame(result["RF"]["P"]).mean(axis=0).values) / y_val.values).mean()
     ax[2].set_title(f"RF\nM.R2: {r2:.4f}\nM.MAPE: {mape:.4f}")
     #
     plt.suptitle(f"Sample {n} / Ftrs {sn}")
@@ -146,14 +153,18 @@ def plot_static(result, n, sn):
 def plot_kmeans(result, c=0.8):
     pred_y, cor_cd, cor_cs, cor_gpt, cor_gtp = [], [], [], [], []
     # best C
+    # cors = []
     for idx in range(data_n):
         # c = (result["Kmeans"]["P"][idx]["Pred_Y"]/result["Kmeans"]["P"][idx]["Act_Y"]).mean()
         # print(c, end=" ", sep=" ")
-        # c = 0
-        # for cor in np.arange(0.1, 1, 0.01):
-        #     if mean_absolute_percentage_error(result["Kmeans"]["P"][idx]["Pred_Y"]*c, y_val.values)<mean_absolute_percentage_error(result["Kmeans"]["P"][idx]["Pred_Y"], y_val.values):
-        #         c = cor
-        # print(c, end=" ", sep=" ")
+        prev_mape = 1
+        print(f"보정전: {c}")
+        for cor in np.arange(0.1, 3, 0.01):
+            if prev_mape > (abs(y_val.values - result["Kmeans"]["P"][0]["Pred_Y"]*cor)/y_val.values).mean():
+                prev_mape = (abs(y_val.values - result["Kmeans"]["P"][idx]["Pred_Y"]*cor) / y_val.values).mean()
+                c = cor
+        # print(f"보정치: {c}")
+        # cors.append(c)
         pred_y.append(result["Kmeans"]["P"][idx]["Pred_Y"]*c)
         cor_cd.append(result["Kmeans"]["P"][idx]["Pred_Y"]*(1-result["Kmeans"]["P"][idx]["CosDist"])*c)
         cor_cs.append(result["Kmeans"]["P"][idx]["Pred_Y"]*(result["Kmeans"]["P"][idx]["CosSim"])*c)
@@ -171,34 +182,40 @@ def plot_kmeans(result, c=0.8):
     ax[0].plot(y_val.values, color="black", linestyle=":")
     ax[0].plot(pred_y.mean(axis=0), color="r")
     r2 = r2_score(y_val.values, pred_y.mean(axis=0))
-    mape = mean_absolute_percentage_error(y_val, pred_y.mean(axis=0))
+    # mape = mean_absolute_percentage_error(y_val, pred_y.mean(axis=0))
+    mape = (abs(y_val.values - pred_y.mean(axis=0)) / y_val.values).mean()
     ax[0].set_title(f"Kmeans\nM.R2: {r2:.4f}\nM.MAPE: {mape:.4f}")
     #
     ax[1].plot(y_val.values, color="black", linestyle=":")
     ax[1].plot(cor_cd.mean(axis=0), color="r")
     r2 = r2_score(y_val.values, cor_cd.mean(axis=0))
-    mape = mean_absolute_percentage_error(y_val, cor_cd.mean(axis=0))
+    # mape = mean_absolute_percentage_error(y_val, cor_cd.mean(axis=0))
+    mape = (abs(y_val.values - cor_cd.mean(axis=0)) / y_val.values).mean()
     ax[1].set_title(f"Kmeans(CD)\nM.R2: {r2:.4f}\nM.MAPE: {mape:.4f}")
     #
     ax[2].plot(y_val.values, color="black", linestyle=":")
     ax[2].plot(cor_cs.mean(axis=0), color="r")
     r2 = r2_score(y_val.values, cor_cs.mean(axis=0))
-    mape = mean_absolute_percentage_error(y_val, cor_cs.mean(axis=0))
+    # mape = mean_absolute_percentage_error(y_val, cor_cs.mean(axis=0))
+    mape = (abs(y_val.values - cor_cs.mean(axis=0)) / y_val.values).mean()
     ax[2].set_title(f"Kmeans(CS)\nM.R2: {r2:.4f}\nM.MAPE: {mape:.4f}")
     #
     ax[3].plot(y_val.values, color="black", linestyle=":")
     ax[3].plot(cor_gpt.mean(axis=0), color="r")
     r2 = r2_score(y_val.values, cor_gpt.mean(axis=0))
-    mape = mean_absolute_percentage_error(y_val, cor_gpt.mean(axis=0))
+    # mape = mean_absolute_percentage_error(y_val, cor_gpt.mean(axis=0))
+    mape = (abs(y_val.values - cor_gpt.mean(axis=0)) / y_val.values).mean()
     ax[3].set_title(f"Kmeans(P/T)\nM.R2: {r2:.4f}\nM.MAPE: {mape:.4f}")
     #
     ax[4].plot(y_val.values, color="black", linestyle=":")
     ax[4].plot(cor_gtp.mean(axis=0), color="r")
     r2 = r2_score(y_val.values, cor_gtp.mean(axis=0))
-    mape = mean_absolute_percentage_error(y_val, cor_gtp.mean(axis=0))
+    # mape = mean_absolute_percentage_error(y_val, cor_gtp.mean(axis=0))
+    mape = (abs(y_val.values - cor_gtp.mean(axis=0)) / y_val.values).mean()
     ax[4].set_title(f"Kmeans(T/P)\nM.R2: {r2:.4f}\nM.MAPE: {mape:.4f}")
     #
     plt.show()
+    return c
     
 # 앙상블 시각화
 def plot_ensemble(result, w, c=0.8):
@@ -225,20 +242,34 @@ def plot_ensemble(result, w, c=0.8):
     ax[0].plot(y_val.values, color="black", linestyle=":")
     ax[0].plot(pred_lr_k, color="r")
     r2 = r2_score(y_val.values, pred_lr_k)
-    mape = mean_absolute_percentage_error(y_val.values, pred_lr_k)
+    # mape = mean_absolute_percentage_error(y_val.values, pred_lr_k)
+    mape = (abs(y_val.values - pred_lr_k) / y_val.values).mean()
     ax[0].set_title(f"LR-K\nM.R2: {r2:.4f}\nM.MAPE: {mape:.4f}")
     #
     ax[1].plot(y_val.values, color="black", linestyle=":")
     ax[1].plot(pred_dt_k, color="r")
     r2 = r2_score(y_val.values, pred_dt_k)
-    mape = mean_absolute_percentage_error(y_val.values, pred_dt_k)
+    # mape = mean_absolute_percentage_error(y_val.values, pred_dt_k)
+    mape = (abs(y_val.values - pred_dt_k) / y_val.values).mean()
     ax[1].set_title(f"DT-K\nM.R2: {r2:.4f}\nM.MAPE: {mape:.4f}")
     #
     ax[2].plot(y_val.values, color="black", linestyle=":")
     ax[2].plot(pred_rf_k, color="r")
     r2 = r2_score(y_val.values, pred_rf_k)
-    mape = mean_absolute_percentage_error(y_val.values, pred_rf_k)
+    # mape = mean_absolute_percentage_error(y_val.values, pred_rf_k)
+    mape = (abs(y_val.values - pred_rf_k) / y_val.values).mean()
     ax[2].set_title(f"LR-K\nM.R2: {r2:.4f}\nM.MAPE: {mape:.4f}")
     #
     plt.suptitle(f"K weight={w}")
     plt.show()
+    
+# Train
+def Train(sample_n):
+    result = train_n(n=sample_n, sn=13)
+    plot_static(result, n=sample_n, sn=13)
+    c = plot_kmeans(result)
+    c = int(c*100)/100
+    print(f"C={c}")
+    plot_ensemble(result, w=0.6, c=c)
+    return result
+        
